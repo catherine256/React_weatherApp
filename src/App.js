1ad389fe4a947398a74810f90ec646bfec23517a
Nakyanzi Catherine <n84252928@china.huawei.com>
2025-07-22 11:30:19 +0300

    added search

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Weather/>
+        <a href='https://github.com/catherine256/React_weatherApp/tree/main' target='_blank'>
+          My Respository
+        </a>
       </header>
     </div>
   );
